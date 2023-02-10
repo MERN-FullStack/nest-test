c5aa0f7479efac2cae2eed5ab8b30fa01404460c
nguyenphucminh <nguyenphucminh.lx@gmail.com>
2023-02-10 23:06:57 +0700

    feat: add api user (create and get)

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // ! add npm i class-transfomer class-validator to validationPipe
+  // https://github.com/typestack/class-validator
+  app.useGlobalPipes(new ValidationPipe())
   await app.listen(3000);
 }
 bootstrap();
